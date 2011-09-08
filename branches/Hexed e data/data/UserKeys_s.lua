d43e209aa340b0c27ee8a3c51f19cc5b9258c1be
anarquia.suporte@gmail.com <anarquia.suporte@gmail.com@4c36811d-3aea-1332-63d3-e94b9120c219>
2011-09-08 03:46:37 +0000

    Revisão 23 :
    Envio de HEXED e data
    
     
    Informações do ADM :
    
    Observações do ADM:

---
+++ branches/Hexed e data/data/UserKeys_s.lua
@@ -0,0 +1,12 @@
+USERKEY_1 = {
+	[9] = { EXE = "Skill bar 2-1", KEY1 = 49 },
+	[10] = { EXE = "Skill bar 2-2", KEY1 = 50 },
+	[11] = { EXE = "Skill bar 2-3", KEY1 = 51 },
+	[12] = { EXE = "Skill bar 2-4", KEY1 = 52 },
+	[13] = { EXE = "Skill bar 2-5", KEY1 = 53 },
+	[14] = { EXE = "Skill bar 2-6", KEY1 = 54 },
+	[15] = { EXE = "Skill bar 2-7", KEY1 = 55 },
+	[16] = { EXE = "Skill bar 2-8", KEY1 = 56 },
+	[17] = { EXE = "Skill bar 2-9", KEY1 = 57 },
+}
+
